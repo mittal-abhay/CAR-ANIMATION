b8ff6c5aea75848e78f468754655a935a5963ccd
ABHAY MITTAL <abhayhmittal@gmail.com>
2022-06-19 14:42:29 +0530

    css error removed

--- commands.bat
+++ commands.bat
@@ -6,6 +6,5 @@ git commit -m "css error removed"
 git remote add origin https://github.com/mittal-abhay/CAR-ANIMATION.git
 git branch
 git checkout -b css_error_removed
+git branch
 git push -u origin css_error_removed
-git status
-git log
