fcffbfc0fbd4959184534e8721c225f7aef4edbe
ABHAY MITTAL <abhayhmittal@gmail.com>
2022-06-19 15:01:12 +0530

    sound fixed by raj

--- commands.bat
+++ commands.bat
@@ -4,6 +4,6 @@ git commit -m "sound fixed by raj"
 git pull
 git remote add origin https://github.com/mittal-abhay/CAR-ANIMATION.git
 git branch
-git checkout -b css_error_removed
+git checkout -b sound_fixed_by_Raj_Sahu
 git branch
-git push -u origin css_error_removed
+git push -u origin sound_fixed_by_Raj_Sahu
